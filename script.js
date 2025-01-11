33f500284c0a0718a5eca519f5f047c500b8c6b8
Lucas G. Costa <112594566+rxluk@users.noreply.github.com>
2025-01-10 23:50:52 -0300

    feat(songs): Songs Animation

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const stressBar = document.getElementById('stress');
     const hungerBar = document.getElementById('hunger');
 
+    const feedSound = new Audio('feed.mp3');
+    const playSound = new Audio('play.mp3');
+    const bathSound = new Audio('bath.mp3');
+    const sleepSound = new Audio('sleep.mp3');
+
     function updateBars(action) {
         messageElement.innerText = '';
         switch(action) {
@@ -20,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 healthBar.value = Math.min(100, healthBar.value + 10);
                 energyBar.value = Math.min(100, energyBar.value + 5);
                 stressBar.value = Math.max(0, stressBar.value - 5);
+                feedSound.play();
                 break;
             case 'play':
                 messageElement.innerText = 'O macaquinho está brincando!';
@@ -27,12 +33,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 energyBar.value = Math.max(0, energyBar.value - 20);
                 healthBar.value = Math.min(100, healthBar.value + 5);
                 stressBar.value = Math.max(0, stressBar.value - 20);
+                playSound.play();
                 break;
             case 'bath':
                 messageElement.innerText = 'O macaquinho está tomando banho!';
                 stressBar.value = Math.max(0, stressBar.value - 20);
                 energyBar.value = Math.min(100, energyBar.value + 5);
                 healthBar.value = Math.min(100, healthBar.value + 5);
+                bathSound.play();
                 break;
             case 'sleep':
                 messageElement.innerText = 'O macaquinho está dormindo!';
@@ -40,6 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 healthBar.value = Math.min(100, healthBar.value + 20);
                 stressBar.value = Math.max(0, stressBar.value - 20);
                 hungerBar.value = Math.min(100, hungerBar.value + 5);
+                sleepSound.play();
                 break;
         }
 
